70a186f1b4e52399525862b36e36e11b6a94060c
Karlan Lemmen <karlanlemmen@gmail.com>
2024-10-30 15:07:55 +0000

    Add features page

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('features/', views.PostList.as_view(), name='features'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
