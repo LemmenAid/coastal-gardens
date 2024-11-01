9aee19ce81b902a5af0fa5c6171a23a9de2973b1
Karlan Lemmen <karlanlemmen@gmail.com>
2024-11-01 11:33:13 +0000

    Add get started to about page

--- about/urls.py
+++ about/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.about_me, name='about'),
+    path('get-started/', views.get_started_view, name='get_started'),
+    path('zone-map/', views.zone_map_view, name='zone_map'),
 ]
--- about/views.py
+++ about/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import About
 
+from django.http import HttpResponse
 # Create your views here.
 
 def about_me(request):
@@ -14,3 +15,9 @@ def about_me(request):
         "about/about.html",
         {"about": about},
     )
+
+def zone_map_view(request):
+    return HttpResponse("Zone Map page is under construction")
+
+def get_started_view(request):
+    return HttpResponse("Get Started page is under construction")
