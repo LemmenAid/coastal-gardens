4793fbc5590d9431ba9a7d58a392782ea75ee7af
Karlan Lemmen <karlanlemmen@gmail.com>
2024-11-06 18:25:24 +0000

    added comment CRUD

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,11 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('features/', views.PostList.as_view(), name='features'),
-    path('<slug:slug>/', views.post_detail, name='post_detail'),
+     path('', views.home, name='home'),
+     path('features/', views.PostList.as_view(), name='features'),
+     path('<slug:slug>/', views.post_detail, name='post_detail'),
+     path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
+     path('<slug:slug>/delete_comment/<int:comment_id>',
+          views.comment_delete, name='comment_delete'),
 ]
