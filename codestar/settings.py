04b1225a7407afc649169101881513d1c0582d54
Karlan Lemmen <karlanlemmen@gmail.com>
2024-11-07 11:28:45 +0000

    Add a CloudinaryImage field to the About model

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
 ALLOWED_HOSTS = ['8000-lemmenaid-coastalgarden-1k1wefnkeaf.ws.codeinstitute-ide.net',
 '.herokuapp.com']
 
