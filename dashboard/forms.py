25d842ba89e0133a436e6f2d4cad8f50bcfb4a97
Karlan Lemmen <karlanlemmen@gmail.com>
2024-11-11 15:21:19 +0000

    Added bootstrap classes to form fields dashboard app

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -5,3 +5,9 @@ class MemberStoryForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["featured_image", "title", "content", "excerpt"]
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'excerpt': forms.Textarea(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'image': forms.FileInput(attrs={'class': 'form-control'}),
+        }
